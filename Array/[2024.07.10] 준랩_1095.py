0e8ca69b59e9490b16bc18ae6ba55a79a257d243
yerim <97941141+yerim123456@users.noreply.github.com>
2024-07-10 21:29:17 +0900

    Update [2024.07.10] 준랩_1095.py

--- Array/[2024.07.10] 준랩_1095.py
+++ Array/[2024.07.10] 준랩_1095.py
@@ -1,4 +1,7 @@
-## 문제 링크: https://joonlab.net/problem.php?id=1095
+# 문제 번호: 준랩_1095
+# 문제 링크: https://joonlab.net/problem.php?id=1095
+# 배운 점: 입력 값에서 값을 빼오는 방식
+# 부족한 점: 순서대로 알고리즘으로 제작하는 연습 & 문제 이해 속도
 
 def solution(n, m, A, B):
     answer = []
