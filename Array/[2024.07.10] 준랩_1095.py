75fffccca5e02baf70f20a261ee6ae04dd6c448f
yerim <97941141+yerim123456@users.noreply.github.com>
2024-07-10 21:27:09 +0900

    Create [2024.07.10] 준랩_1095.py

---
+++ Array/[2024.07.10] 준랩_1095.py
@@ -0,0 +1,18 @@
+## 문제 링크: https://joonlab.net/problem.php?id=1095
+
+def solution(n, m, A, B):
+    answer = []
+    for k in B:
+        cnt = 0
+        for a in A:
+            if a >= k:
+                cnt += 1
+        answer.append(cnt)
+    return answer
+
+n, m = map(int, input().split())
+A = list(map(int, input().split()))
+B = list(int(input()) for _ in range(m))
+C = solution(n,m,A,B)
+for c in C:
+    print(c)
