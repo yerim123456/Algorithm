7181537695d7205fb6a022d62cb9840eff14fd5f
yerim <97941141+yerim123456@users.noreply.github.com>
2024-12-23 22:55:06 +0900

    [level 0] Title: 문자열 곱하기, Time: 0.03 ms, Memory: 84.8 MB -BaekjoonHub

--- 프로그래머스/0/181940. 문자열 곱하기/문자열 곱하기.java
+++ 프로그래머스/0/181940. 문자열 곱하기/문자열 곱하기.java
@@ -1,11 +1,18 @@
+// repeat(repeat count)
+
 class Solution {
     public String solution(String my_string, int k) {
         String answer = "";
         
+        /** 1.
         for(int i = 0; i < k; i++){
-        answer += my_string;
+            answer += my_string;
         }
+        **/
         
+        // 2.
+        answer = my_string.repeat(k);
+
         return answer;
     }
 }
