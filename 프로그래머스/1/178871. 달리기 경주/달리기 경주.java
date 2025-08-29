541156582a98650947ece9e04fdd825a1ed65213
yerim <97941141+yerim123456@users.noreply.github.com>
2025-08-29 23:27:36 +0900

    [level 1] Title: 달리기 경주, Time: 262.02 ms, Memory: 374 MB -BaekjoonHub

---
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.java
@@ -0,0 +1,27 @@
+import java.util.HashMap;
+
+
+class Solution {
+    public String[] solution(String[] players, String[] callings) {
+        HashMap<String, Integer> playerIndex = new HashMap<>();
+        for (int i = 0; i < players.length; i++) {
+            playerIndex.put(players[i], i);
+        }
+        
+        for (String calling : callings) {
+            int idx = playerIndex.get(calling);
+            if (idx > 0) {
+                // swap
+                String temp = players[idx - 1];
+                players[idx - 1] = players[idx];
+                players[idx] = temp;
+
+                // Map 갱신
+                playerIndex.put(players[idx], idx);
+                playerIndex.put(players[idx - 1], idx - 1);
+            }
+        }
+
+        return players;
+    }
+}
